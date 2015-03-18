5ae305ea4e1c0fd4f9455b3240cd9a345b117231
Dale Palmer & J. TURNER <student@epicodus.com>
2015-03-18 14:15:55 -0700

    Modify 'user' spec; add files.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-
 # RSpec.describe User, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
 
@@ -7,5 +6,6 @@ require 'rails_helper'
     it { should validate_presence_of :email}
     it { should validate_presence_of :password}
     it { should validate_presence_of :password_confirmation}
-    
+    it { should have_many :photos}
+
 end
